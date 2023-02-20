249f93a0705d44507797fda1581017b5abd21e42
hunorszegedi <szegedihunorofficial@gmail.com>
2023-02-20 10:21:10 +0200

    lab2 updated

--- labor-02/src/oop/labor02/Main.java
+++ labor-02/src/oop/labor02/Main.java
@@ -2,6 +2,6 @@ package oop.labor02;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("labor02");
+        System.out.println("labor02 updated");
     }
 }
