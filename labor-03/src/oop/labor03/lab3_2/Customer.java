bdce29c888db2b91750353eb8b6fcae74bb468b0
hunorszegedi <szegedihunorofficial@gmail.com>
2023-03-05 22:13:40 +0200

    lab3 done_lab_3

--- labor-03/src/oop/labor03/lab3_2/Customer.java
+++ labor-03/src/oop/labor03/lab3_2/Customer.java
@@ -2,54 +2,84 @@ package oop.labor03.lab3_2;
 
 import oop.labor03.lab3_1.BankAccount;
 
-import java.util.Arrays;
-
 public class Customer {
     private String firstName;
     private String lastName;
-    // constant
     public static final int MAX_ACCOUNTS = 10;
-    // number of accounts
     private int numAccounts;
-    // an array for the accounts
     private BankAccount accounts[] = new BankAccount[MAX_ACCOUNTS];
 
+    //konstruktor
     public Customer(String pFirstName, String pLastName) {
         firstName = pFirstName;
         lastName = pLastName;
     }
 
+    //metodusok
+    //1
+    public void addAccount(BankAccount account) {
+        if (numAccounts == MAX_ACCOUNTS) {
+            System.out.println("LIMIT REACHED!\n");
+        } else {
+            accounts[numAccounts] = account;
+            numAccounts++;
+        }
+    }
+
+    //2
+    public BankAccount getAccount(String accountNumber) {
+        for (int i = 0; i < numAccounts; i++) {
+            if (accounts[i].getAccountNumber().equals(accountNumber)) {
+                return accounts[i];
+            }
+        }
+        return null;
+    }
+
+    //3
+    public Integer getNumAccounts() {
+        return numAccounts;
+    }
+
+    //4
     public String getFirstName() {
         return firstName;
     }
 
+    //5
     public String getLastName() {
         return lastName;
     }
 
-    public void addAccount(BankAccount pAccount) {
-        if (numAccounts < MAX_ACCOUNTS) {
-            for (int i = 0; i < numAccounts; i++) {
-                if (accounts[i] != null) {
-                    accounts[i] = pAccount;
-                    break;
-                }
-            }
-        }
-
-    }
-
+    //6
     public void setLastName(String pLastName) {
         lastName = pLastName;
     }
 
+    //7
+    public void closeAccount(String accountNumber) {
+        int original = numAccounts;
+        for (int i = 0; i < numAccounts; i++) {
+            if (accounts[i].getAccountNumber().equals(accountNumber)) {
+                accounts[i] = accounts[numAccounts - 1];
+                accounts[numAccounts - 1] = null;
+                numAccounts--;
+                break;
+            }
+        }
+        if(original == numAccounts){
+            System.out.println("Doesn't exist!\n");
+        }
+    }
+
+    //8
     @Override
     public String toString() {
-        return "Customer{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", numAccounts=" + numAccounts +
-                ", accounts=" + Arrays.toString(accounts) +
-                '}';
+        StringBuffer result = new StringBuffer();
+        result.append(firstName + ' ' + lastName + " accounts:\n");
+        for (int i = 0; i < numAccounts; ++i) {
+            result.append("\t" + accounts[i] + "\n");
+        }
+        return result.toString();
     }
 }
--- labor-03/src/oop/labor03/lab3_2/Main.java
+++ labor-03/src/oop/labor03/lab3_2/Main.java
@@ -1,17 +1,65 @@
 package oop.labor03.lab3_2;
 
 import oop.labor03.lab3_1.BankAccount;
-import oop.labor03.lab3_2.Customer;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        //creating customer
         Customer customer1 = new Customer("Szegedi", "Hunor");
-        customer1.addAccount(new BankAccount("OTP001"));
-        //add account
-        System.out.println(customer1);
+        Customer customer2 = new Customer("Nagy", "Janos");
 
+        BankAccount account1 = new BankAccount("IBAN0001");
+        BankAccount account2 = new BankAccount("IBAN0002");
+        BankAccount account3 = new BankAccount("IBAN0003");
+        BankAccount account4 = new BankAccount("IBAN0004");
+        BankAccount account5 = new BankAccount("IBAN0005");
+        BankAccount account6 = new BankAccount("IBAN0006");
+        BankAccount account7 = new BankAccount("IBAN0007");
+        BankAccount account8 = new BankAccount("IBAN0008");
+        BankAccount account9 = new BankAccount("IBAN0009");
+
+        //customer1 -- 5 accounts
+        customer1.addAccount(account9);
+        customer1.addAccount(account8);
+        customer1.addAccount(account7);
+        customer1.addAccount(account6);
+        customer1.addAccount(account5);
+
+        //customer2 -- 9 accounts
+        customer2.addAccount(account1);
+        customer2.addAccount(account2);
+        customer2.addAccount(account3);
+        customer2.addAccount(account4);
+        customer2.addAccount(account5);
+        customer2.addAccount(account6);
+        customer2.addAccount(account7);
+        customer2.addAccount(account8);
+        customer2.addAccount(account9);
+
+        //print the customers
+        System.out.println(customer1);
+        System.out.println(customer2);
+
+        //deposit in each account
+        account1.deposit(11);
+        account2.deposit(230);
+        account3.deposit(9123);
+        account4.deposit(123);
+        account5.deposit(30);
+        account6.deposit(990);
+        account7.deposit(540);
+        account8.deposit(650);
+        account9.deposit(80);
+
+        //close the first account of the first customer
+        customer1.closeAccount("IBAN0009");
+        //close the last account of the second customer
+        customer2.closeAccount("IBAN0009");
+
+        //print the customers
+        System.out.println(customer1);
+        System.out.println(customer2);
 
     }
 }
