83c780c270d1665c9d6b565ab5ff7d4f1919463d
hunorszegedi <szegedihunorofficial@gmail.com>
2023-05-10 19:07:55 +0300

    lab11 Product class done

---
+++ labor-11/src/oop/labor11/lab11_3/Product.java
@@ -0,0 +1,50 @@
+package oop.labor11.lab11_3;
+
+public class Product implements Comparable<Product>{
+    private int identifier;
+    private String name;
+    private int amount;
+    private int price;
+
+    public Product(int identifier, String name, int amount, int price) {
+        this.identifier = identifier;
+        this.name = name;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void increaseAmount(int newAmount) {
+        this.amount += newAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "identifier=" + identifier +
+                ", name='" + name + '\'' +
+                ", amount=" + amount +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Product other) {
+        return Integer.compare(identifier, other.identifier);
+    }
+}
